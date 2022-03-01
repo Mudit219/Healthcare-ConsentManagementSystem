27e0925f818eebf1eec5d971d190def48190f150
Saksham Agarwal <saksham.aladin@gmail.com>
2022-03-01 10:05:04 +0530

    Adding Simple Backend and API for Documentation

---
+++ Backend/demo/src/main/java/com/example/demo/customer/GreetingController.java
@@ -0,0 +1,21 @@
+
+package com.example.demo.customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting get_greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
