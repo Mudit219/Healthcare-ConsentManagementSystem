582ef0956881a26de300296cf43893d19a3ef76b
Mudit <mudit219@gmail.com>
2023-05-07 19:10:30 +0530

    Add Multiple Testcases

--- Database/DumpHospitalData.sql
+++ Database/DumpHospitalData.sql
@@ -1,11 +1,44 @@
 use fortis_hospital_db;
 INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
-VALUES (17,"70424638979356",'Fortis Hospital','Saksham','Rahul','Nexium','Ulcer','9545323431','#DC6521','2020-11-19');
-
-use narayana_hospital_db;
-INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
-VALUES (17,"70424638979356",'Narayana Hospital','Saksham','Rahul','Crocine','Headache','9545323431','#DC6521','2020-11-19');
+VALUES (11,"70424638979356",'Fortis Hospital','Saksham','Rahul','Nexium','Ulcer','9545323431','#DC6521','2020-11-19');
 
 use kavery_hospital_db;
 INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
-VALUES (17,"70424638979356",'Kavery Hospital','Saksham','Rahul','D-Cold','Cold and Cough','9545323431','#DC6521','2020-11-19');
+VALUES (19,"70424638979356",'Kavery Hospital','Saksham','Rahul','Crestor','Cholestrol','9545323431','#DC6521','2022-06-21');
+
+use narayana_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (71,"70424638979356",'Narayana Hospital','Saksham','Renjith','Crocine','Headache','9545323431','#DC7865','2022-05-27');
+
+use narayana_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (89,"70424638979356",'Narayana Hospital','Saksham','Renjith','Dolo','Fever','9545323431','#DC7865','2022-08-05');
+
+use kavery_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (25,"70424638979356",'Kavery Hospital','Saksham','Renjith','Dolo','Fever','9545323431','#DC7865','2022-09-07');
+
+use kavery_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (31,"70424638979356",'Kavery Hospital','Saksham','Renjith','D-Cold','Cold and Cough','9545323431','#DC7865','2023-04-15');
+
+use fortis_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (75,"70424638979356",'Fortis Hospital','Saksham','Sanjay','Nexium','Ulcer','9545323431','#DC2122','2023-05-04');
+
+use kavery_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (41,"70424638979356",'Kavery Hospital','Mudit','Rahul','Crestor','Cholestrol','9545323431','#DC6521','2022-11-02');
+
+use kavery_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (57,"70424638979356",'Kavery Hospital','Mudit','Rahul','Nexium','Stomach Pain','9545323431','#DC6521','2023-03-17');
+
+use fortis_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (39,"70424638979356",'Fortis Hospital','Mudit','Sanjay','Nexium','Ulcer','9545323431','#DC2122','2022-07-16');
+
+use kavery_hospital_db;
+INSERT INTO `ehealth_records` (ehr_id,abha_id,hospital_name,patient_name,doctor_name,prescription,diagnosis,patient_phone,doctor_license,date)
+VALUES (91,"70424638979356",'Kavery Hospital','Mudit','Renjith','Dolo','Fever','9545323431','#DC7865','2022-11-21');
+
